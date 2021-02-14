0157d08fee812ff455957e5cf740357b500dee55
venkateshveeroju <veerojuvenkatesh5@gmail.com>
2021-02-14 11:42:02 +0100

    updated base url

--- angular/src/app/_services/esgdetails.service.ts
+++ angular/src/app/_services/esgdetails.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-const baseUrl = 'http://localhost:8080/api/esgdetalis';
+const baseUrl = 'http://localhost:8080/api/esgdetails';
 
 @Injectable({
     providedIn: 'root'
