9d17170b3fc68a8d1303f432281e84b2103516d5
venkateshveeroju <veerojuvenkatesh5@gmail.com>
2021-02-16 10:45:44 +0100

    Added Email feature

--- java/src/main/java/com/esgreport/controller/UsersController.java
+++ java/src/main/java/com/esgreport/controller/UsersController.java
@@ -1,36 +1,12 @@
 package com.esgreport.controller;
 
-import java.io.UnsupportedEncodingException;
-
-import javax.mail.MessagingException;
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.esgreport.entity.User;
-import com.esgreport.service.UserServices;
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
-@RequestMapping("/api/email")
+@RequestMapping("")
 public class UsersController {
-	 @Autowired
-	    private UserServices service;
 	 
-	     
-	    @PostMapping("/process_register")
-	    public String processRegister(User user, HttpServletRequest request)
-	            throws UnsupportedEncodingException, MessagingException {
-	        service.register(user, getSiteURL(request));       
-	        return "register_success";
-	    }
-	     
-	    private String getSiteURL(HttpServletRequest request) {
-	        String siteURL = request.getRequestURL().toString();
-	        return siteURL.replace(request.getServletPath(), "");
-	    }  
 	     
 }
