447b40887f041d0d3d9d4aa0ef7af2982814bb60
venkateshveeroju <veerojuvenkatesh5@gmail.com>
2021-03-08 12:28:32 +0100

    updated

---
+++ java/src/main/java/com/esgreport/model/EsgDetailsDelegateUserModel.java
@@ -0,0 +1,35 @@
+package com.esgreport.model;
+
+public class EsgDetailsDelegateUserModel {
+
+	private int txtorganizationdelegateuser;
+	private int txtactivitiesdelegateuser;
+	private int txtheadquartersdelegateuser;
+	private int txtoperationsdelegateuser;
+	public int getTxtorganizationdelegateuser() {
+		return txtorganizationdelegateuser;
+	}
+	public void setTxtorganizationdelegateuser(int txtorganizationdelegateuser) {
+		this.txtorganizationdelegateuser = txtorganizationdelegateuser;
+	}
+	public int getTxtactivitiesdelegateuser() {
+		return txtactivitiesdelegateuser;
+	}
+	public void setTxtactivitiesdelegateuser(int txtactivitiesdelegateuser) {
+		this.txtactivitiesdelegateuser = txtactivitiesdelegateuser;
+	}
+	public int getTxtheadquartersdelegateuser() {
+		return txtheadquartersdelegateuser;
+	}
+	public void setTxtheadquartersdelegateuser(int txtheadquartersdelegateuser) {
+		this.txtheadquartersdelegateuser = txtheadquartersdelegateuser;
+	}
+	public int getTxtoperationsdelegateuser() {
+		return txtoperationsdelegateuser;
+	}
+	public void setTxtoperationsdelegateuser(int txtoperationsdelegateuser) {
+		this.txtoperationsdelegateuser = txtoperationsdelegateuser;
+	}
+	
+	
+}
