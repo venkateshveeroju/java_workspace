cdbc6d8b331f24fe6047c00e0d1f92f3b28c38df
venkateshveeroju <veerojuvenkatesh5@gmail.com>
2021-03-28 16:50:55 +0200

    updated file name

--- java/src/main/java/com/esgreport/entity/EsgdetailsUserStatus.java
+++ java/src/main/java/com/esgreport/entity/EsgdetailsUserStatus.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+
 @Entity
 @Table(name = "esgdetail_user_status")
 public class EsgDetailsUserStatus {
